87e958ad9440837118f64274384eebaa8e682064
Buck Golemon <buck@yelp.com>
2016-03-14 10:24:50 -0700

    try-coverage comments

--- try-coverage.sh
+++ try-coverage.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # This script sometimes fails when run concurrently with itself.  (see demo.sh)
 set -eu
+
+# arguments/environment, with defaults
+SEQ="${1:-0}"
 export TOP=$(dirname $(readlink -f $0))
 export LOGS=$PWD/logs-${NOW:-$(date +"%F-%T.%N")}
-SEQ="${1:-0}"
+
+# make a clean directory for our demo
 DEMO=$LOGS/$SEQ
 rm -rf $DEMO
 mkdir -p $DEMO
 cd $DEMO
-
 echo $DEMO
 
 # run coverage, with all the debugging options I could find enabled.
@@ -20,6 +23,7 @@ PYTHONPATH=$TOP \
     $TOP/try_execnet.py \
     > test.log 2>&1
 
+# assert that at least one of the coverage files has coverage for myscript
 for cov in cov.*; do
     export COVERAGE_FILE=$cov
     if coverage report --fail-under 100 $TOP/myscript.py > ${COVERAGE_FILE}.report; then
@@ -27,6 +31,7 @@ for cov in cov.*; do
     fi
 done
 
+# there is no coverage for myscript!
 echo "
 
 ===============================================================================

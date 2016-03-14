400f9dfc992055aea9dc05ce0a4cdb3c77a8bde1
Buck Golemon <buck@yelp.com>
2016-03-14 10:08:10 -0700

    mv one-thread -> try-coverage

--- demo.sh
+++ demo.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-set -eux
-
-trap 'find -name failure' ERR
+set -eu
 
 export TOP="$PWD"
 export LOGS=$PWD/logs-$(date +"%F-%T.%N")
+trap 'find $LOGS -name failure' ERR
+
+# run 200 times, in batches of ten parallel processes
+# on my machine, the script takes about 1 second to run
+#   with a failure rate around 5%
+#   so we usually find a failure in 1-3 seconds
 time seq 200 |
-    xargs -n1 -P10 ./one-thread.sh
+    xargs -n1 -P10 ./try-coverage.sh
